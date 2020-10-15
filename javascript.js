12cce8e064734142516517c521c783546568bd06
leckaraxmuffin <71823717+leckaraxmuffin@users.noreply.github.com>
2020-10-15 12:53:43 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,10 @@
+let main = '<div onclick="web()"><h1 id="h1">Hallo</h1></div>';
+let bye = '<div onclick="webb()"><h1 id="h1">Tschüss</h1></div>';
 
-function hallo(){
-    document.getElementById('h1').style.backgroundColor = 'gray';
+function web(){
+    document.getElementById('body').innerHTML = bye;
+}
+
+function webb(){
+    document.getElementById('body').innerHTML = main;
 }
